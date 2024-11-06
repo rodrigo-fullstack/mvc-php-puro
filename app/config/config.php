31a15ef65095ba862e45fe76af3afa0a082a29d8
rodrigo-fullstack <rodrigopereirafaria9@gmail.com>
2024-11-06 11:18:51 -0300

    refactor (db): Mudando nome do bd e tabela

--- app/config/config.php
+++ app/config/config.php
@@ -6,7 +6,7 @@
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
-define('DB_NAME', 'livrobd');
+define('DB_NAME', 'bookbd');
 define('DB_PORT', '3306');
 
 
---
+++ app/sql/rename-table.sql
@@ -0,0 +1,1 @@
+RENAME TABLE livro TO book;
