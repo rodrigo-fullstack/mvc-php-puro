b7d5114b64267b57521987c3816694d44128a449
rodrigo-fullstack <rodrigopereirafaria9@gmail.com>
2024-11-06 11:18:27 -0300

    test (db-dsn): Testando Db Dsn

--- app/models/Database.php
+++ app/models/Database.php
@@ -1,5 +1,26 @@
 <?php
+include '../config/config.php';
 
 class Database{
-    
+    //Definindo propriedades privadas com valores do BD capturados de config.php
+    private $host = DB_HOST;
+    private $user = DB_USER;
+    private $passwd = DB_PASS;
+    private $dbname = DB_NAME;
+    private $dbport = DB_PORT;
+
+    private $dbh; //Manipulador do BD
+    private $stmt; //Script SQL
+    private $error; //Mensagem de Erro
+
+    //Construtores inicializam as instâncias da classe: Iniciar conexão com BD
+    public function __construct(){
+
+        //Data Source Name, nome que será fornecido no PDO
+        $dsn = "mysql:host={$this->host};dbname={$this->dbname};port={$this->dbport}";
+        echo $dsn;
+    }   
 }
+
+$db = new Database();
+
